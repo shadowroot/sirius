30ddaed5ef8fc010aab9ef94623b1d22b4967be8
Jan Vlnas <git@jan.vlnas.cz>
2014-12-26 14:46:45 +0100

    Use Pliny's CastingConfigHelpers

--- config/config.rb
+++ config/config.rb
@@ -1,7 +1,6 @@
-require "pliny/config_helpers"
+require 'pliny/config_helpers'
 
-# Access all config keys like the following:
-#
+# Access all config keys like the following, #
 #     Config.database_url
 #
 # Each accessor corresponds directly to an ENV key, which has the same name
@@ -11,36 +10,33 @@ require "pliny/config_helpers"
 # different type. Make sure to typecast any values that need to be something
 # else (i.e. `.to_i`).
 module Config
-  extend Pliny::ConfigHelpers
+  extend Pliny::CastingConfigHelpers
 
   # Mandatory -- exception is raised for these variables when missing.
-  mandatory \
-    :database_url,
-    :kosapi_oauth_client_id,
-    :kosapi_oauth_client_secret,
-    :domain
+  mandatory :database_url, string
+  mandatory :kosapi_oauth_client_id, string
+  mandatory :kosapi_oauth_client_secret, string
+  mandatory :domain, string
 
   # Optional -- value is returned or `nil` if it wasn't present.
-  optional \
-    :console_banner,
-    :placeholder,
-    :versioning_default,
-    :versioning_app_name,
-    :sentry_dsn,
-    :sync_schedule
+  optional :console_banner, string
+  optional :placeholder, string
+  optional :versioning_default, string
+  optional :versioning_app_name, string
+  optional :sentry_dsn, string
+  optional :sync_schedule, string
 
   # Override -- value is returned or the set default. Remember to typecast.
-  override \
-    db_pool:          5,
-    port:             5000,
-    rack_env:         'development',
-    raise_errors:     'false',
-    root:             File.expand_path("../../", __FILE__),
-    timeout:          45,
-    force_ssl:        'true',
-    versioning:       'false',
-    tz:               'Europe/Prague',
-    puma_max_threads: 16,
-    puma_min_threads: 1,
-    puma_workers:     3
+  override :db_pool, 5, int
+  override :port, 5000, int
+  override :rack_env, 'development', string
+  override :raise_errors, false, bool
+  override :root, File.expand_path('../../', __FILE__), string
+  override :timeout, 45, int
+  override :force_ssl, true, bool
+  override :versioning, false, bool
+  override :tz, 'Europe/Prague', string
+  override :puma_max_threads, 16, int
+  override :puma_min_threads, 1, int
+  override :puma_workers, 3
 end
